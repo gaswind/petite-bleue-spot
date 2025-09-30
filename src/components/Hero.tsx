f59ff0388df0c5d8a4648b3e62f98f03c9b0b154
Gaspard Wibaux <gasp.wibaux@gmail.com>
2025-09-30 16:04:18 +0200

    suppr arrow

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,11 +35,6 @@ const Hero = () => {
             <a href="#histoire">Découvrir notre histoire</a>
           </Button>
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <ArrowDown className="h-6 w-6 text-white" />
-        </div>
       </div>
     </section>
   );
